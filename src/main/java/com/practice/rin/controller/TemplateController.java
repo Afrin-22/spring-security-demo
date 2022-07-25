5af6e5ec61555fb05d35b91b99628fc4776ae202
afrin.zakir <Thaleb@98>
2022-07-25 22:17:32 +0530

    Fix template error

--- src/main/java/com/practice/rin/controller/TemplateController.java
+++ src/main/java/com/practice/rin/controller/TemplateController.java
@@ -3,13 +3,14 @@ package com.practice.rin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
 public class TemplateController {
 
-    @GetMapping("login")
+    @GetMapping("/login")
     public String getLogin() {
-        return "login.html";
+        return "login";
     }
 }
